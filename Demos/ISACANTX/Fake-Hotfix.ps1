0989f0930730259918b3c67df8a95ff110d9d98f
AuditClay <42394776+AuditClay@users.noreply.github.com>
2020-11-06 14:52:28 -0600

    Hotfixes

---
+++ Demos/ISACANTX/Fake-Hotfix.ps1
@@ -0,0 +1,34 @@
+﻿$day1 = (Get-Date -hour 0 -Minute 0 -Second 0).AddDays(6-((Get-Date).DayOfWeek.value__) )
+$day0 = $friday.AddDays(-364)
+$patchPct = 0.1
+$numServers = 1000
+$maxPatch = 10
+
+for( $w=0; $w -lt 52; $w++)
+{
+    $d = $day0.AddDays(7*$w)
+
+    for( $i = 1; $i -lt $numServers; ++$i )
+    {
+        $serverName = "Server" + $i
+        if( (Get-Random -Minimum 0.0 -Maximum 1.0) -le $patchPct)
+        {
+            $r = Get-Random -Minimum 1 -Maximum $maxPatch
+            for( $p = 0; $p -lt $r; $p++ )
+            {     
+                $kb = Get-Random -Minimum 1000000 -Maximum 9999999
+                $patchResult = [PSCustomObject]@{
+                    Source = $serverName
+                    InstalledOn = $d
+                    HotFixID = "MSKB" + $kb
+                    Description = "Security Update", "Update" | Get-Random
+                    InstalledBy = "NT AUTHORITY\SYSTEM"
+                }
+                $patchResult
+            }
+        }
+    }
+
+}
+
+#Source, Description, HotFixID, InstalledBy, InstalledOn
